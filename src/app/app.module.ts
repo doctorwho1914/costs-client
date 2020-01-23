2428ab48104134575b3e4cf53702725c6ef544d2
Andrii Malishcshuk <mr.malishuk@gmail.com>
2020-01-23 21:42:55 +0200

    notifications and clear form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {CostsService} from './services/costs.service';
-import {HttpClientModule} from '@angular/common/http';
-import {CategoryService} from './services/category.service';
-import {CostListComponent} from './cost-list/cost-list.component';
-import {CostAddComponent} from './cost-add/cost-add.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CostsService } from './services/costs.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CategoryService } from './services/category.service';
+import { CostListComponent } from './cost-list/cost-list.component';
+import { CostAddComponent } from './cost-add/cost-add.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,8 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     ReactiveFormsModule,
     NgbModule,
     FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [
     CostsService,
--- src/app/cost-add/cost-add.component.ts
+++ src/app/cost-add/cost-add.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {CostsService} from '../services/costs.service';
 import {CategoryService} from '../services/category.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-cost-add',
@@ -17,6 +18,7 @@ export class CostAddComponent {
   constructor(
     private costsService: CostsService,
     private categoryService: CategoryService,
+    private toastr: ToastrService,
   ) {
     this.addCostForm = new FormGroup({
       categoryId: new FormControl(''),
@@ -28,6 +30,7 @@ export class CostAddComponent {
   }
 
   load() {
+    this.addCostForm.reset();
     this.categoryService.list()
       .then(data => {
         console.log(data);
@@ -39,6 +42,7 @@ export class CostAddComponent {
     if (confirm('Добавить трату?')) {
       this.costsService.add(this.addCostForm.value)
         .then(e => {
+          this.toastr.success('Готово');
           this.load();
         });
     }
